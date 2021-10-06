931c7e865dad2a9000d4c01c23aa2e936de73156
Tudor42 <puscas.tudor42@gmail.com>
2021-10-06 18:51:27 +0300

    adaugare problema numarul 5

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#problema 3, 4
+#problema 3, 4, 5
 
 def is_prime(n):
   """
@@ -30,7 +30,7 @@ def get_goldbach(n):
     None - in caz contrar
   """
   if n < 4: #numerele mai mici de 4 nu pot fi
-    #reprezenteate ca suma de 2 numere prime 
+    # reprezenteate ca suma de 2 numere prime
     return None
 
   if n == 4:
@@ -52,25 +52,25 @@ def get_goldbach(n):
 
 def test_get_goldbach():
   print("Test 1")
-  assert get_goldbach(1)          == None, "should be None"
+  assert get_goldbach(1) is None, "should be None"
   print("Test 15")
-  assert get_goldbach(15)         == [2, 13], "should be [2, 13]"
+  assert get_goldbach(15) == [2, 13], "should be [2, 13]"
   print("Test 21")
-  assert get_goldbach(21)         == [2, 19], "should be [2, 19]"
+  assert get_goldbach(21) == [2, 19], "should be [2, 19]"
   print("Test 24")
-  assert get_goldbach(24)         == [5, 19], "should be [5, 19]"
+  assert get_goldbach(24) == [5, 19], "should be [5, 19]"
   print("Test 28")
-  assert get_goldbach(28)         == [5, 23], "should be [5, 23]"
+  assert get_goldbach(28) == [5, 23], "should be [5, 23]"
   print("Test 97")
-  assert get_goldbach(97)         == None, "should be None"
+  assert get_goldbach(97) is None, "should be None"
   print("Test 100")
-  assert get_goldbach(100)        == [3, 97], "should be [3, 97]"
+  assert get_goldbach(100) == [3, 97], "should be [3, 97]"
   print("Test 10000")
   assert get_goldbach(10000)      == [59, 9941], "should be [59, 9941]"
   print("Test 80000")
   assert get_goldbach(80000)      == [3, 79997], "should be [3, 79997]"
-  print("Test 1000000000")
-  assert get_goldbach(1000000000) == [71, 999999929], "should be [71, 999999929]"
+  print("Test 100000000")
+  assert get_goldbach(100000000) == [11, 99999989], "should be [71, 999999929]"
   print("Everything passed")
   
 
@@ -110,21 +110,45 @@ def test_get_newton_sqrt():
   assert round(get_newton_sqrt(50000, 11), 6) == 223.606798
   print("Everything passed")
 
+def is_palindrome(n) -> bool:
+  n = str(n)
+  m = n[-1::-1]
+  if m == n:
+    return True
+  return False
+
+def test_is_palindrome():
+  print('Test 0')
+  assert is_palindrome(0) is True
+  print('Test 54')
+  assert is_palindrome(54) is False
+  print('Test 131')
+  assert is_palindrome(131) is True
+  print('Test 109312')
+  assert is_palindrome(109312) is False
+  print('Test 199979991')
+  assert is_palindrome(199979991) is True
+  print('Everything passed')
+
 def main():
+  print('###')
+  print('Se testeaza get_newton_sqrt:')
+  test_get_newton_sqrt()
+  print('###')
+  print('Se testeaza get_goldbach')
+  test_get_goldbach()
+  print('###')
+  print('Se testeaza is_palindrome')
+  test_is_palindrome()
+  print()
   print('Scrie help pentru a vedea lista de comenzi')
-  quit = False
-  while not quit:
+  while True:
     command = input('$').split()
     if command[0] == 'help':
       print('get_goldbach [n]            - genereaza p1 si p2 prime astfel incat n = p1 + p2')
-      print('test_get_goldbach           - testeaza functia get_goldbach')
       print('get_newton_sqrt [n] [steps] - calculeaza radicalul lui n dupa algoritmul lui newton')
-      print('test_get_newton_sqrt        - testeaza functia get_newton_sqrt')
+      print('is_palindrome [n]           - verifica daca un numar este palindrom')
       print('quit                        - opreste programul')
-
-    if command[0] == 'test_get_newton_sqrt':
-      test_get_newton_sqrt()
-
     if command[0] == 'get_newton_sqrt':
       if len(command) < 3:
         print('prea putini argumenti')
@@ -138,10 +162,6 @@ def main():
         print(get_newton_sqrt(float(command[1]), steps))
       except:
         print('n trebuie sa fie un numar')
-
-    if command[0] == 'test_get_goldbach':
-      test_get_goldbach()
-
     if command[0] == 'get_goldbach':
       if len(command) < 2:
         print('prea putini argumenti')
@@ -149,9 +169,14 @@ def main():
         print(get_goldbach(int(command[1])))
       except:
         print('argumentul trebuie sa fie numar intreg')
+    if command[0] == 'is_palindrome':
+      if len(command) < 2:
+        print('prea putini argumenti')
+        continue
+      print(is_palindrome(command[1]))
 
     if command[0] == 'quit':
-      quit = True
+      break
 
 if __name__ == '__main__':
   main()
